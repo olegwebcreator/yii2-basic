ce37e099fcfd18b2a2864a4db8e8b58ed52d1ef7
System <oleg.webcreator@yandex.ru>
2016-03-10 21:12:48 +0300

    Extended config system

---
+++ config/web.php
@@ -0,0 +1,30 @@
+<?php
+$config = [
+    'id' => 'app',
+    'components' => [
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'request' => [
+            'cookieValidationKey' => 'K8lGnrajW-24KrRNyQRSK4ubGBJWQ0YF',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+        ],
+    ],
+];
+ 
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+ 
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+ 
+return $config;
