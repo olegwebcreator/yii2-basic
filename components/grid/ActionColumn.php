742a41e19b053088e388af98805937da343dd2c9
System <oleg.webcreator@yandex.ru>
2016-03-12 00:30:54 +0300

    not that

---
+++ components/grid/ActionColumn.php
@@ -0,0 +1,8 @@
+<?php
+namespace app\components\grid;
+class ActionColumn extends \yii\grid\ActionColumn
+{
+    public $contentOptions = [
+        'class' => 'action-column',
+    ];
+}
---
+++ tests/codeception/config/config-local.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Application configuration shared by all test types
+ */
+return [
+    'components' => [
+        'db' => [
+            'dsn' => 'mysql:host=localhost;dbname=yii2_basic_tests',
+        ],
+    ],
+];
